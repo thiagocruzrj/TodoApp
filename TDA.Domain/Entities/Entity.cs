9f9468b445bc32ee934fd25e94fb67852bb70f61
Thiago Cruz <thagocruz@gmail.com>
2020-08-06 19:32:34 -0300

    Add base entity

---
+++ TDA.Domain/Entities/Entity.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TDA.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
