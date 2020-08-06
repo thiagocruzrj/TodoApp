2b0fb602c28af491fa664b196eb319dbbb520315
Thiago Cruz <thagocruz@gmail.com>
2020-08-06 19:47:34 -0300

    Rename entity and add constructor

--- TDA.Domain/Entities/TodoItem.cs
+++ TDA.Domain/Entities/TodoItem.cs
@@ -2,8 +2,16 @@ using System;
 
 namespace TDA.Domain.Entities
 {
-    public class Todo : Entity
+    public class TodoItem : Entity
     {
+        public TodoItem(string title, DateTime date, string user)
+        {
+            Title = title;
+            Done = false;
+            Date = date;
+            User = user;
+        }
+
         public string Title { get; private set; }
         public bool Done { get; private set; }
         public DateTime Date { get; private set; }
