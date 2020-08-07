ee638ee100d8c61632559c6adfb22521a7758a9a
Thiago Cruz <thagocruz@gmail.com>
2020-08-07 19:54:50 -0300

    Add updateTodo handler

--- TDA.Domain/Handlers/TodoHandler.cs
+++ TDA.Domain/Handlers/TodoHandler.cs
@@ -3,9 +3,14 @@ using TDA.Domain.Commands.Contracts;
 
 namespace TDA.Domain.Handlers.Contracts
 {
-    public class TodoHandler : IHandler<CreateTodoCommand>
+    public class TodoHandler : IHandler<CreateTodoCommand>, IHandler<UpdateTodoCommand>
     {
         public ICommandResult Handler(CreateTodoCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICommandResult Handler(UpdateTodoCommand command)
         {
             throw new System.NotImplementedException();
         }
