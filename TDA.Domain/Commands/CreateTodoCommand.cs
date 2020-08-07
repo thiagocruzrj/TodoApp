31000d7dfceedd32fcbdcba78ed6ab18777fc632
Thiago Cruz <thagocruz@gmail.com>
2020-08-07 16:51:55 -0300

    Add

--- TDA.Domain/Commands/CreateTodoCommand.cs
+++ TDA.Domain/Commands/CreateTodoCommand.cs
@@ -1,9 +1,10 @@
 using System;
+using Flunt.Notifications;
 using TDA.Domain.Commands.Contracts;
 
 namespace TDA.Domain.Commands
 {
-    public class CreateTodoCommand : ICommand
+    public class CreateTodoCommand : Notifiable, ICommand
     {
         public CreateTodoCommand() { }
         public CreateTodoCommand(string title, string user, DateTime date)
@@ -16,5 +17,10 @@ namespace TDA.Domain.Commands
         public string Title { get; set; }
         public string User { get; set; }
         public DateTime Date { get; set; }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
