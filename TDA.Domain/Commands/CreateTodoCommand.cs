0c0af95a0966219c83845941dedac155dd632157
Thiago Cruz <thagocruz@gmail.com>
2020-08-07 16:43:26 -0300

    Add ctor parameterless and with all properties to test

--- TDA.Domain/Commands/CreateTodoCommand.cs
+++ TDA.Domain/Commands/CreateTodoCommand.cs
@@ -1,9 +1,18 @@
 using System;
+using TDA.Domain.Commands.Contracts;
 
 namespace TDA.Domain.Commands
 {
-    public class CreateTodoCommand
+    public class CreateTodoCommand : ICommand
     {
+        public CreateTodoCommand() { }
+        public CreateTodoCommand(string title, string user, DateTime date)
+        {
+            Title = title;
+            User = user;
+            Date = date;
+        }
+        
         public string Title { get; set; }
         public string User { get; set; }
         public DateTime Date { get; set; }
