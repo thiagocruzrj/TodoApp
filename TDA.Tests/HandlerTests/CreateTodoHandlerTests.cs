d2467dac58b80bfe07ecf6cfbe15bdbd7ecbe3f5
Thiago Cruz <thagocruz@gmail.com>
2020-08-08 19:47:00 -0300

    Red

--- TDA.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ TDA.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -8,13 +8,13 @@ namespace TDA.Tests.HandlerTests
         [TestMethod]
         public void If_an_invalid_command_should_stop_the_execution()
         {
-
+            Assert.Fail();
         }
 
         [TestMethod]
         public void If_a_valid_command_should_continue_the_execution()
         {
-            
+            Assert.Fail();
         }
     }
 }
