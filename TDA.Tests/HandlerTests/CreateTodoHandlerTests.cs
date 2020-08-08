4bc90778fd42cc789a2f15d6ea1f951eea8086c7
Thiago Cruz <thagocruz@gmail.com>
2020-08-08 20:09:22 -0300

    Refact

--- TDA.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ TDA.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -10,21 +10,27 @@ namespace TDA.Tests.HandlerTests
     [TestClass]
     public class CreateTodoHandlerTests
     {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("","", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Command Title test", "TestUser", DateTime.Now);
+        private readonly TodoHandler _todoHandler = new TodoHandler(new FakeTodoRepository());
+
+        public CreateTodoHandlerTests()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+
         [TestMethod]
         public void If_an_invalid_command_should_stop_the_execution()
         {
-            var command = new CreateTodoCommand("", "", DateTime.Now);
-            var handler = new TodoHandler(new FakeTodoRepository());
-            var result = (GenericCommandResult)handler.Handler(command);
+            var result = (GenericCommandResult)_todoHandler.Handler(_invalidCommand);
             Assert.AreEqual(result.Success, false);
         }
 
         [TestMethod]
         public void If_a_valid_command_should_continue_the_execution()
         {
-            var command = new CreateTodoCommand("Command Title Test", "UserTest", DateTime.Now);
-            var handler = new TodoHandler(new FakeTodoRepository());
-            var result = (GenericCommandResult)handler.Handler(command);
+            var result = (GenericCommandResult)_todoHandler.Handler(_validCommand);
             Assert.AreEqual(result.Success, true);
         }
     }
