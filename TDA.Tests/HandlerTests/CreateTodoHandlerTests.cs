6027eb600cf811f73bdccb5b63407a3991cbddd9
Thiago Cruz <thagocruz@gmail.com>
2020-08-08 19:45:57 -0300

    Create CreateTodoHandlerTests.cs

---
+++ TDA.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDA.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+        [TestMethod]
+        public void If_an_invalid_command_should_stop_the_execution()
+        {
+
+        }
+
+        [TestMethod]
+        public void If_a_valid_command_should_continue_the_execution()
+        {
+            
+        }
+    }
+}
