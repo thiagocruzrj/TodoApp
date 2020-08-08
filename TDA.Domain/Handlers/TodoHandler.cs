d1477f40250bb5892fca71405235328f88f3d891
Thiago Cruz <thagocruz@gmail.com>
2020-08-08 19:12:46 -0300

    Finish creating a task

--- TDA.Domain/Handlers/TodoHandler.cs
+++ TDA.Domain/Handlers/TodoHandler.cs
@@ -1,6 +1,7 @@
 using Flunt.Notifications;
 using TDA.Domain.Commands;
 using TDA.Domain.Commands.Contracts;
+using TDA.Domain.Entities;
 using TDA.Domain.Repositories;
 
 namespace TDA.Domain.Handlers.Contracts
@@ -19,6 +20,12 @@ namespace TDA.Domain.Handlers.Contracts
             command.Validate();
             if (command.Invalid)
                 return new GenericCommandResult(false, "Ops, your task is invalid!", command.Notifications);
+
+            var todo = new TodoItem(command.Title, command.Date, command.User);
+
+            _repository.Create(todo);
+
+            return new GenericCommandResult(true, "Task saved", todo.Id);
         }
     }
 }
