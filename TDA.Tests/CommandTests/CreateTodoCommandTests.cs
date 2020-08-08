662be869b9c0501323508fa37b47cdb4fdfc45e9
Thiago Cruz <thagocruz@gmail.com>
2020-08-08 19:22:29 -0300

    Red on both testes

--- TDA.Tests/CommandTests/CreateTodoCommandTests.cs
+++ TDA.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -1,7 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace TDA.Tests.CommandTests
 {
+    [TestClass]
     public class CreateTodoCommandTests
     {
-        
+        [TestMethod]
+        public void If_an_invalid_command()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void If_a_valid_command()
+        {
+            Assert.Fail();
+        }
     }
 }
