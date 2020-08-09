2c6aa6ae5b130b3fae207c1d7879e5b468821811
Thiago Cruz <thagocruz@gmail.com>
2020-08-09 17:39:12 -0300

    Create TodoItemTest.cs

---
+++ TDA.Tests/EntitiesTest/TodoItemTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDA.Tests.EntitiesTest
+{
+    [TestClass]
+    public class TodoItemTest
+    {
+        [TestMethod]
+        public void Has_new_todo_done_should_concluded()
+        {
+            Assert.Fail();
+        }
+    }
+}
