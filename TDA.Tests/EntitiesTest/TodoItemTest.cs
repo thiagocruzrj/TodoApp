d2b5d8b5dad048f56e657787d4ead8724071f171
Thiago Cruz <thagocruz@gmail.com>
2020-08-09 17:42:42 -0300

    Refect

--- TDA.Tests/EntitiesTest/TodoItemTest.cs
+++ TDA.Tests/EntitiesTest/TodoItemTest.cs
@@ -1,14 +1,17 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDA.Domain.Entities;
 
 namespace TDA.Tests.EntitiesTest
 {
     [TestClass]
     public class TodoItemTest
     {
+        private readonly TodoItem _todoItem = new TodoItem("Test Tittle", DateTime.Now, "Test User");
         [TestMethod]
         public void Has_new_todo_done_should_concluded()
         {
-            Assert.Fail();
+            Assert.AreEqual(_todoItem.Done, false);
         }
     }
 }
