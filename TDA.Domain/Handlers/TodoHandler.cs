3c904363730da1605d447435c3fdc8e7bbcdb5b6
Thiago Cruz <thagocruz@gmail.com>
2020-08-10 18:50:38 -0300

    Implement MarkTodoAsUndoneCommand handler method

--- TDA.Domain/Handlers/TodoHandler.cs
+++ TDA.Domain/Handlers/TodoHandler.cs
@@ -45,7 +45,17 @@ namespace TDA.Domain.Handlers.Contracts
 
         public ICommandResult Handler(MarkTodoAsUndoneCommand command)
         {
-            throw new System.NotImplementedException();
+            command.Validate();
+            if(command.Invalid)
+                return new GenericCommandResult(false, "Ops, your task is invalid!", command.Notifications);
+
+            var todo = _repository.GetById(command.Id, command.User);
+
+            todo.MarkAsDone();
+
+            _repository.Update(todo);
+
+            return new GenericCommandResult(true, "Task saved", todo); 
         }
 
         public ICommandResult Handler(MarkTodoAsDoneCommand command)
