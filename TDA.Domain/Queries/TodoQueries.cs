178c2f7712cd3afe86aed66f3470184a6c5692bf
Thiago Cruz <thagocruz@gmail.com>
2020-08-10 20:48:27 -0300

    Create TodoQueries.cs

---
+++ TDA.Domain/Queries/TodoQueries.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq.Expressions;
+using TDA.Domain.Entities;
+
+namespace TDA.Domain.Queries
+{
+    public static class TodoQueries
+    {
+        public static Expression<Func<TodoItem, bool>> GetAll(string user)
+        {
+            return x => x.User == user;
+        }
+    }
+}
