8aed61a70104a817417a22778e0cfc115c79beb4
Thiago Cruz <thagocruz@gmail.com>
2020-08-10 20:53:14 -0300

    Add GetByPeriod query

--- TDA.Domain/Queries/TodoQueries.cs
+++ TDA.Domain/Queries/TodoQueries.cs
@@ -16,9 +16,14 @@ namespace TDA.Domain.Queries
             return x => x.User == user && x.Done;
         }
 
-                public static Expression<Func<TodoItem, bool>> GetAllUndone(string user)
+        public static Expression<Func<TodoItem, bool>> GetAllUndone(string user)
         {
             return x => x.User == user && x.Done == false;
+        }
+
+        public static Expression<Func<TodoItem, bool>> GetByPeriod(string user, DateTime date, bool done)
+        {
+            return x=> x.User == user && x.Done == done && x.Date.Date == date.Date;
         }
     }
 }
