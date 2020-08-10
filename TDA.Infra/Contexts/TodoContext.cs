66af904311966414b7c06fb613a158d790e7ad0c
Thiago Cruz <thagocruz@gmail.com>
2020-08-10 19:22:55 -0300

    Override todoItem modelCreating

--- TDA.Infra/Contexts/TodoContext.cs
+++ TDA.Infra/Contexts/TodoContext.cs
@@ -8,5 +8,15 @@ namespace TDA.Infra.Contexts
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<TodoItem> Todos { get; set; }
+
+        protected override OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().Property(x => x.Id);
+            modelBuilder.Entity<TodoItem>().Property(x => x.User);
+            modelBuilder.Entity<TodoItem>().Property(x => x.Title);
+            modelBuilder.Entity<TodoItem>().Property(x => x.Done);
+            modelBuilder.Entity<TodoItem>().Property(x => x.Date);
+            modelBuilder.Entity<TodoItem>().Property(x => x.User);
+        }
     }
 }
