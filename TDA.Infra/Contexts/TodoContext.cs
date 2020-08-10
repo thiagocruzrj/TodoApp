efeb8855d88ba373e3086aa20a3483086883b963
Thiago Cruz <thagocruz@gmail.com>
2020-08-10 19:18:51 -0300

    Mapping table

--- TDA.Infra/Contexts/TodoContext.cs
+++ TDA.Infra/Contexts/TodoContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using TDA.Domain.Entities;
 
 namespace TDA.Infra.Contexts
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<TodoItem> Todos { get; set; }
     }
 }
