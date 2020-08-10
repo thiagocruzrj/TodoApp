1676839eea3e98c42438f9df5c4016363e1776b3
Thiago Cruz <thagocruz@gmail.com>
2020-08-10 19:30:02 -0300

    Add design package

--- TDA.Infra/Contexts/TodoContext.cs
+++ TDA.Infra/Contexts/TodoContext.cs
@@ -9,12 +9,13 @@ namespace TDA.Infra.Contexts
 
         public DbSet<TodoItem> Todos { get; set; }
 
-        protected override OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<TodoItem>().ToTable("Todo");
             modelBuilder.Entity<TodoItem>().Property(x => x.Id);
-            modelBuilder.Entity<TodoItem>().Property(x => x.User);
-            modelBuilder.Entity<TodoItem>().Property(x => x.Title);
-            modelBuilder.Entity<TodoItem>().Property(x => x.Done);
+            modelBuilder.Entity<TodoItem>().Property(x => x.User).HasMaxLength(120).HasColumnType("varchar(120)");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Title).HasMaxLength(160).HasColumnType("varchar(160)");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Done).HasColumnType("bit");
             modelBuilder.Entity<TodoItem>().Property(x => x.Date);
             modelBuilder.Entity<TodoItem>().Property(x => x.User);
         }
