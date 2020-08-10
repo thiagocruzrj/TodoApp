d3a9f95e600fb9923a0c6121ad43306cb05625b3
Thiago Cruz <thagocruz@gmail.com>
2020-08-10 20:56:36 -0300

    Create TodoQueryTests.cs

---
+++ TDA.Tests/QueryTests/TodoQueryTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDA.Tests.QueryTests
+{
+    [TestClass]
+    public class TodoQueryTests
+    {
+        [TestMethod]
+        public void When_execute_query_should_return_only_user_task()
+        {
+            Assert.Fail();
+        }
+    }
+}
