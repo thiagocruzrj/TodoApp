da3f7e547e93b67e0063251010cf7a2c0b901cf5
Thiago Cruz <thagocruz@gmail.com>
2020-08-10 22:35:01 -0300

    Create TodoController.cs

---
+++ TDA.Api/Controllers/TodoController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TDA.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
+    {
+        
+    }
+}
