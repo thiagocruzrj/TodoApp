06df24dbdb4b53b4180ffc6bd648916c67db7c4d
Thiago Cruz <thagocruz@gmail.com>
2020-08-10 21:47:54 -0300

    Create TodoRepository.cs

---
+++ TDA.Infra/Repositories/TodoRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using TDA.Domain.Entities;
+using TDA.Domain.Repositories;
+
+namespace TDA.Infra.Repositories
+{
+    public class TodoRepository : ITodoRepository
+    {
+    }
+}
